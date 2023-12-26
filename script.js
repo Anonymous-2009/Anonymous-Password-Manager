7de2bd4da04315f86bf0875dbe5a38c859f645a7
Anonymous <krishnabag751@gmail.com>
2023-12-26 11:36:24 +0000

    work done later

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
+// logic to fill table 
+const deletePassword = () => {
+  let data = localStorage.getItem("passwords");
+  let array = JSON.parse(data);
+}
+
+const showPasswords = () => { 
+let table = document.querySelector('table');
+
+let data = localStorage.getItem("passwords");
+
+if(data == null){
+   table.innerHTML = "No passwords saved";
+} else {
+  let array = JSON.parse(data);
+  let string = "";
+  for (let index = 0; index < array.length; index++) {
+     let element = array[index];
+      string += `
+      <tr>
+        <td>${element.website}</td>
+        <td>${element.username}</td> 
+        <td>${element.password}</td>
+        <td>${"delete"}</td>
+          </tr>`; 
+  }
+table.innerHTML = table.inerrHTML + string;  
+}
+}
+showPasswords();
+let website = document.querySelector('#website');
+
 let username = document.getElementById('username');
 
 let password = document.getElementById('password');
 
 let button = document.querySelector(".btn");
+
 button.addEventListener("click", (event) => {
   event.preventDefault()
   event.stopPropagation()
@@ -11,14 +44,22 @@ button.addEventListener("click", (event) => {
   let passwords = localStorage.getItem("passwords");
     if (passwords == null) {
        let json = []
-json.push({username:username.value,password:password.value});
+json.push({username:username.value,password:password.value,website:website.value});
     alert("password saved");
     localStorage.setItem("passwords", JSON.stringify(json));
     } else {
+      let table = document.querySelector('table');
+      table.innerHTML = `
+      <tr>
+          <th>Website</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Delete</th>
+      </tr> `;
        let json = JSON.parse(localStorage.getItem("passwords"));
-json.push({username:username.value,password:password.value});
+json.push({username:username.value,password:password.value,website:website.value});;
     alert("password saved");
     localStorage.setItem("passwords", JSON.stringify(json));
     }
-                                       
+    showPasswords();                                  
 })
